a104b4fe24c2e70fd5e6712bb5a63ebd308dffc8
Jona2010 <jaguirres@ulasalle.edu.pe>
2026-04-20 14:43:17 -0500

    fix server.js

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Crear pool conexión MySQL con datos de .env o valores por defecto
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "190.237.242.22",
+  host: process.env.DB_HOST || "mysql.us.cloudlogin.co",
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   database: process.env.DB_NAME || "intelliall_apps",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
+  user: process.env.DB_USER || "intelliall_apps",
+  password: process.env.DB_PASS || "426896",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
