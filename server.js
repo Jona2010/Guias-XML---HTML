b15840d04df77a0f7493b59aa2c6fc8dbaa1291a
Jona2010 <jaguirres@ulasalle.edu.pe>
2026-04-20 12:19:02 -0500

    fix express wildcard route

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 // Servir archivos estáticos del frontend
-app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // Crear pool conexión MySQL con datos de .env o valores por defecto
 const pool = mysql.createPool({
